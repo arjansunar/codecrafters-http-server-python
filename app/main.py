76f364fca07aa79161395e9a5be0e7711fc6353d
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-11-05 20:01:07 +0545

    feat: send 200 ok response via connection

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 import socket  # noqa: F401
 
 
+CRLF = "\r\n"
+
+
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    conn, _ = server_socket.accept()  # wait for client
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    conn.sendall(response)
 
 
 if __name__ == "__main__":
