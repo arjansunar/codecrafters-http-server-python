3d9948e09fb42b5b23f322ee6302da1f0e09953e
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-11-05 20:27:52 +0545

    feat: use regex to parse out method and target

--- app/main.py
+++ app/main.py
@@ -1,14 +1,38 @@
+import re
 import socket  # noqa: F401
 
 
 CRLF = "\r\n"
 
+REQUEST_MATCHER = r"(?P<method>GET|POST) (?P<target>/[\w.]+)"
+
+
+def response_builder(status: int, reason_phrase: str, version="HTTP/1.1"):
+    return f"{version} {status} {reason_phrase}{CRLF}{CRLF}"
+
+
+AVAILABLE_ROUTES = [
+    "/",
+]
+
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, _ = server_socket.accept()  # wait for client
-    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    message = conn.recv(1024).decode()
+    request = message.split(CRLF)
+    match = re.search(REQUEST_MATCHER, request[0])
+    if match:
+        grouped = match.groupdict()
+        known_route = grouped.get("target") in AVAILABLE_ROUTES
+        if not known_route:
+            response = response_builder(404, "Not Found").encode()
+        else:
+            response = response_builder(200, "OK").encode()
+    else:
+        response = response_builder(500, "Server Error").encode()
     conn.sendall(response)
+    conn.close()
 
 
 if __name__ == "__main__":
